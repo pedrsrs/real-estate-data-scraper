c3cc2b3f0e102a3c5268e09b4f857fbcfdaf07fc
pedrsrs <pedrosrs.dev@gmail.com>
2024-01-09 23:37:34 -0300

    Update information parsing

--- property_scraper.py
+++ property_scraper.py
@@ -1,5 +1,7 @@
-from selenium import webdriver
 import time
+import re
+import sqlite3
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -17,31 +19,76 @@ def get_elements(driver, url, class_name):
 
 def extract_information(element):
     title = element.find_element(By.CSS_SELECTOR, "h2").text
-    price = element.find_element(By.CSS_SELECTOR, "h3.olx-text.olx-text--body-large.olx-text--block.olx-text--semibold.olx-ad-card__price").text
+    unparsed_property_price = element.find_element(By.CSS_SELECTOR, "h3.olx-text.olx-text--body-large.olx-text--block.olx-text--semibold.olx-ad-card__price").text
     location = element.find_element(By.CSS_SELECTOR, "div.olx-ad-card__location-date-container > p").text
     data = element.find_element(By.CSS_SELECTOR, "p.olx-ad-card__date--horizontal").text
 
     other_prices_elements = element.find_elements(By.CSS_SELECTOR, "div.olx-ad-card__priceinfo.olx-ad-card__priceinfo--horizontal > p")
     other_prices = [el.text for el in other_prices_elements]
 
+    iptu, condominio = parse_other_prices(other_prices)
+
     spans = element.find_elements(By.CSS_SELECTOR, 'li.olx-ad-card__labels-item > span')
     labels = [span.get_attribute('aria-label') for span in spans]
 
-    return title, price, location, data, other_prices, labels
+    quartos, metros_quadrados, vagas_garagem, banheiros = parse_information(labels)
 
+    property_price = parse_property_price(unparsed_property_price)
+    neighborhood = parse_location(location)
+    return title, property_price, neighborhood, data, other_prices, iptu, condominio, quartos, metros_quadrados, vagas_garagem, banheiros, labels
 
-def print_information(title, price, location, data, other_prices, labels):
+def parse_property_price(unparsed_property_price):
+    property_price_digits = re.findall(r'\d+', unparsed_property_price)
+    concatenated_price = ''.join(property_price_digits)
+    return int(concatenated_price)
+
+def parse_other_prices(other_prices):
+    iptu = condominio = None
+
+    for item in other_prices:
+        if 'IPTU' in item:
+            iptu = int(item.split()[-1].replace(".", ""))
+        elif 'Condomínio' in item:
+            condominio = int(item.split()[-1].replace(".", ""))
+
+    return iptu, condominio
+
+def parse_information(other_prices):
+    quartos = metros_quadrados = vagas_garagem = banheiros = None
+
+    for item in other_prices:
+        if 'quarto' in item:
+            quartos = int(item.split()[0])
+        elif 'metro' in item:
+            metros_quadrados = int(item.split()[0])
+        elif 'garagem' in item:
+            vagas_garagem = int(item.split()[0])
+        elif 'banheiro' in item:
+            banheiros = int(item.split()[0])
+
+    return quartos, metros_quadrados, vagas_garagem, banheiros
+
+def parse_location(location):
+    neighborhood = location.split(", ")[1]
+    return neighborhood
+
+def print_information(title, price, neighborhood, data, iptu, condominio, quartos, metros_quadrados, vagas_garagem, banheiros):
     print(title)
-    print(price)
-    print(other_prices)
-    print(location)
+    print("Property price: " + str(price))
+    print("IPTU: " + str(iptu))
+    print("Condominio: " + str(condominio))
+    print("Quartos: " + str(quartos))
+    print("Metros quadrados: " + str(metros_quadrados))
+    print("Vagas garagem: " + str(vagas_garagem))
+    print("Banheiros: " + str(banheiros))
+    print("Bairro: " + neighborhood)
     print(data)
-    print(labels)
     print("-" * 10)
     time.sleep(0.5)
 
 def main():
     driver = initialize_driver()
+    
 
     url = 'https://www.olx.com.br/imoveis/venda/estado-sp/sao-paulo-e-regiao/zona-sul?pe=550000&ps=500001'
     class_name = 'renderIfVisible'
